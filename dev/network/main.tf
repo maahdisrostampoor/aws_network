49fbb43825c4dcd2bd762f2d80122e2b2ee0f0b3
Mahdis <maahdisrostampoor@gmail.com>
2024-03-05 23:28:08 -0500

    Adding my network module into Remote

--- dev/network/main.tf
+++ dev/network/main.tf
@@ -2,7 +2,7 @@
 # Module to deploy basic networking 
 module "vpc-dev" {
   #source = "../../aws_network"
-  source = "github.com/maahdisrostampoor/lab4.git"
+  source = "github.com/maahdisrostampoor/aws_network.git"
   env                = var.env
   vpc_cidr           = var.vpc_cidr
   public_cidr_blocks = var.public_subnet_cidrs
