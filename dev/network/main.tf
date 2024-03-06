60cca523a50087e27f597da6a32a085d92681ea4
Mahdis <maahdisrostampoor@gmail.com>
2024-03-06 00:04:24 -0500

    Reset submodule to a valid commit

--- dev/network/main.tf
+++ dev/network/main.tf
@@ -3,7 +3,7 @@
 module "vpc-dev" {
   #source = "../../aws_network"
   source = "github.com/maahdisrostampoor/aws_network.git"
-  env                = var.env
+  env                = var.env 
   vpc_cidr           = var.vpc_cidr
   public_cidr_blocks = var.public_subnet_cidrs
   prefix             = var.prefix
